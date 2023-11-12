573c1dc4a25561e959d19d057669f65cbdec63e9
ReverseCall <r.alisson123987@gmail.com>
2023-11-12 09:27:25 -0300

    uff

--- Scalie/settings.py
+++ Scalie/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x0^!!$csw=pg!l6#uyro@n^%t=x66qz_&bca10x&)hh-e3ala^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
